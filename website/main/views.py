886d595ee9202253f7dade3dde28107aaf0f6336
DanShinde <pravin113347@gmail.com>
2023-02-18 18:31:36 +0530

    creatingAuto app

---
+++ website/main/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render,redirect
+from .forms import PostForm, RegisterForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from .models import Post
+
+
+# Create your views here.
+@login_required(login_url="/login")
+def home(request):
+    posts = Post.objects.all()
+
+    if request.method == "POST":
+        post_id = request.POST.get('post-id')
+        post = Post.objects.filter(id=post_id).first()
+        if post and post.author == request.user:
+            post.delete()
+
+    return render(request, 'main/home.html', {'posts': posts})
+
+
+@login_required(login_url="/login")
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('/home')
+
+    else:
+        form = PostForm()
+    
+    return render(request,'main/create_post.html', {'form': form})
+
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/home')
+    else:
+        form = RegisterForm()
+    return render(request,'registration/sign_up.html', {'form': form})
