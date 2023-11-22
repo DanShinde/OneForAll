78483b3f9c900136b015179a44301ac93a3cfd21
DanShinde <DanShinde@github.com>
2023-11-22 17:54:47 +0530

    url added for rockwell alarms

--- textgen/urls.py
+++ textgen/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from textgen.views import homeIO, upload, upload8, io_mapping, server_error
-
+from textgen.views import ExcelImportView, XMLImportView
 urlpatterns = [
     path('homeIO', homeIO, name='homeIO'),
     path('upload', upload, name='upload'),
     path('upload8', upload8, name='upload8'),
     path('io_mapping', io_mapping, name='io_mapping'),
     path('server_error', server_error, name='server_error'),
+    path('import-excel/', ExcelImportView.as_view(), name='import-excel'),
+    path('import-xml/', XMLImportView.as_view(), name='import-xml'),
+
 ]
 
