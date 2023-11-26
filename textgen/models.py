e762a62bc25adc10fbc4a6f8b43a005d32f7a669
DanShinde <DanShinde@github.com>
2023-11-26 17:14:21 +0530

    updated trigger length

--- textgen/models.py
+++ textgen/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Alarm(models.Model):
     triggerID = models.CharField(max_length=10)
     messageID = models.CharField(max_length=10)
-    trigger = models.CharField(max_length=50)
+    trigger = models.CharField(max_length=100)
     message = models.CharField(max_length=100)
 
